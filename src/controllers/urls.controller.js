5827dd8e437dc58c7457b4916d922491915cf58e
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-08-07 11:44:01 -0300

    Feature: urls/open get

--- src/controllers/urls.controller.js
+++ src/controllers/urls.controller.js
@@ -10,8 +10,8 @@ export async function urlsShorthen(req,res){
         const iduseronline = (await db.query(`SELECT id FROM useronline WHERE token=$1;`,[token])).rows[0].id
         
 
-        await db.query(`INSERT INTO shorturl (iduseronline,shorturl,url,visitcount) VALUES ($1,$2,$3,$4)`,[iduseronline,shortUrl,url,0])
-        const response = (await db.query(`SELECT * from shorturl WHERE shorturl=$1`,[shortUrl])).rows[0]
+        await db.query(`INSERT INTO shorturl (iduseronline,"shortUrl",url,visitcount) VALUES ($1,$2,$3,$4)`,[iduseronline,shortUrl,url,0])
+        const response = (await db.query(`SELECT * from shorturl WHERE "shortUrl"=$1`,[shortUrl])).rows[0]
 
         res.status(201).send({id:response.id,shortUrl:response.shorturl})
     }catch(err){
@@ -19,14 +19,30 @@ export async function urlsShorthen(req,res){
     }
 }
 
-
 export async function urlsbyid(req,res){
     const {id} = req.params
 
     try{
-        const response = await db.query(`SELECT id,shorturl,url FROM shorturl WHERE id=$1`,[id])
+        const response = await db.query(`SELECT id,"shortUrl",url FROM shorturl WHERE id=$1`,[id])
+
         if (response.rowCount === 0) return res.sendStatus(404)
-        res.send(response.rows[0])
+        
+        res.status(200).send(response.rows[0])
+    }catch(err){
+        res.status(500).send(err.message)
+    }
+}
+
+export async function openShort(req,res){
+    const {shortUrl} = req.params 
+
+    try{
+        const response = (await db.query(`SELECT * FROM shorturl WHERE "shortUrl"= $1`,[shortUrl]))
+        if (response.rowCount === 0) res.sendStatus(404);
+
+        const visita = response.rows[0].visitCount+1
+        await db.query(`UPDATE shorturl SET "visitCount"=$1 WHERE "shortUrl"= $2`,[visita,shortUrl])
+        res.redirect(response.rows[0].url)
     }catch(err){
         res.status(500).send(err.message)
     }
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import validateSchema from "../middlewares/validateSchema.js";
 import { urlsShema } from "../schemas/urlShorthen.shema.js";
-import { urlsShorthen, urlsbyid } from "../controllers/urls.controller.js";
+import { openShort, urlsShorthen, urlsbyid } from "../controllers/urls.controller.js";
 import { autorization } from "../middlewares/authorization.js";
 
 const urlRoute = Router();
 
 urlRoute.post('/urls/shorten',autorization,validateSchema(urlsShema),urlsShorthen)
 urlRoute.get('/urls/:id',urlsbyid)
+urlRoute.get('/urls/open/:shortUrl',openShort)
 
 export default urlRoute;
