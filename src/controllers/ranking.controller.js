d530c9a6791adad9f21ed3bb48e871556659758c
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-08-07 17:36:53 -0300

    fix: ranking 2.0

--- src/controllers/ranking.controller.js
+++ src/controllers/ranking.controller.js
@@ -1,9 +1,34 @@
 import { db } from "../database/database.connection.js";
 
 export async function rankingget(req,res){
+    
 
     try{
-        res.status(200).send((await db.query(`SELECT id,name,linksCount,visitCount FROM shorturl ORDER BY "visitCount" DESC LIMIT 10;`)).rows)
+        let arrfinal = []
+        const arr =[] 
+        const response = (await db.query(`SELECT * FROM useronline;`)).rows
+
+        for (let i=0;i<response.length;i++){
+            const name = (await db.query(`SELECT name FROM Usersignup WHERE id = $1`,[response[i].iduser])).rows[0].name;
+            const linksCount = (await db.query(`SELECT * FROM shorturl WHERE iduseronline=$1`,[response[i].id])).rowCount;
+            const listavisitas = (await db.query(`SELECT "visitCount" FROM shorturl WHERE iduseronline=$1`,[response[i].id])).rows
+            let soma = 0
+            listavisitas.forEach((i)=>{
+                soma += i.visitCount
+            })
+            
+            arrfinal.push({id:response[i].id,name,linksCount,visitCount:soma})
+            arr.push(soma)
+
+        }
+
+        arrfinal = arrfinal.sort((a,b)=>{
+            return b.visitCount-a.visitCount
+        })
+        
+        
+
+        res.status(200).send(arrfinal.splice(0,10))
     }catch(err){
         res.status(500).send(err.message);
     }
