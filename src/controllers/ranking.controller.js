c9c5657b2d161a9565a46a142a1e573047ae7c26
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-08-07 16:46:47 -0300

    fix: ranking

--- src/controllers/ranking.controller.js
+++ src/controllers/ranking.controller.js
@@ -3,7 +3,7 @@ import { db } from "../database/database.connection.js";
 export async function rankingget(req,res){
 
     try{
-        res.status(200).send((await db.query(`SELECT * FROM shorturl ORDER BY "visitCount" DESC LIMIT 10;`)).rows)
+        res.status(200).send((await db.query(`SELECT id,name,linksCount,visitCount FROM shorturl ORDER BY "visitCount" DESC LIMIT 10;`)).rows)
     }catch(err){
         res.status(500).send(err.message);
     }
